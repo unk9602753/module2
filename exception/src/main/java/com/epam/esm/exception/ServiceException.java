666be1bc98f9c6906250cfde84b24a7bc64c3f6c
unk9602753 <n.kuleshruslan@gmail.com>
2022-06-08 09:52:47 +0300

    add module exception

---
+++ exception/src/main/java/com/epam/esm/exception/ServiceException.java
@@ -0,0 +1,38 @@
+package com.epam.esm.exception;
+
+public class ServiceException extends Exception {
+    private long id;
+    private String concreteMessage;
+
+    private ErrorCode errorCode;
+
+    public ServiceException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ServiceException(ErrorCode errorCode, long id){
+        this.errorCode=errorCode;
+        this.id = id;
+    }
+
+    public ServiceException(ErrorCode errorCode, String concreteMessage){
+        this.errorCode=errorCode;
+        this.concreteMessage = concreteMessage;
+    }
+
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public String getId() {
+        if (id != 0) {
+            return String.valueOf(id);
+        }
+        return "";
+    }
+
+    public String getConcreteMessage() {
+        return concreteMessage != null ? concreteMessage : "";
+    }
+}
